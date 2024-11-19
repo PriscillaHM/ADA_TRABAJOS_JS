ae491291f254d27504c3037fd09c89f76bdbfba7
PriscillaHM <al20760537@ite.edu.mx>
2024-11-18 22:30:20 -0800

    Ejercicio de la clase 9

---
+++ clase9/nombres_vinculantes.js
@@ -0,0 +1,11 @@
+//Ejemplo de nombres de vinculación
+let catch22 = "Piki";
+let _cariable = 10;
+let $total = 100;
+
+
+//Ejemplos no validos
+//let if = true; 
+// let user@name = 'piki';
+// let 8id = 'piki';
+
