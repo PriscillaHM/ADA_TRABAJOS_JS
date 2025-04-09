24b57b3408f7e766b8541af94666322c6b4a106d
PriscillaHM <al20760537@ite.edu.mx>
2025-04-08 20:36:32 -0700

    Ejercicio 6 de la clase 1

--- MODULO3_TS/clase1/ejercicios/ejercicio6.ts
+++ MODULO3_TS/clase1/ejercicios/ejercicio6.ts
@@ -3,5 +3,18 @@
 // variable, y utiliza el operador typeof para verificar el tipo antes de imprimir los 
 // valores. 
 
-let unknown: unknown = 'Hola soy unknow'
-console.log(unknown)
+let unknown: unknown 
+
+unknown = 'Hola mundo'
+if(typeof unknown === 'string'){
+    console.log('Es un string: ',unknown)
+}
+
+unknown = 42
+if(typeof unknown === 'number'){
+    console.log('Es un number: ',unknown)
+}
+unknown = true;
+if (typeof unknown === "boolean") {
+  console.log("Es un booleano:", unknown);
+}
