559d607708bece69b296c7805bc68d417e75e485
PriscillaHM <al20760537@ite.edu.mx>
2025-04-08 20:38:54 -0700

    ejercicio 8 de clase 2

--- MODULO3_TS/clase2/ejercicios/ejercicio8.ts
+++ MODULO3_TS/clase2/ejercicios/ejercicio8.ts
@@ -3,17 +3,33 @@
 // ("circulo" o "rectangulo") y luego acepte los parámetros necesarios para cada 
 // tipo de figura (radio para el círculo y largo y ancho para el rectángulo). La 
 // función debe retornar el área de la figura. Usa tipos específicos para cada caso. 
-// type figura = 'circulo' | 'rectangulo'
-// let areaFigura: figura = 'circulo'
-const calcularArea = (areaFigura: string, radio?: number, base?: number, altura?: number) => {
-    let area: number = 0
-    if(areaFigura == 'circulo'){
-        area = Math.PI * (radio * radio)
-        return `El area del circulo es ${area}`
-    } else {
+
+type Figura = 'circulo' | 'rectangulo'
+
+const calcularArea = (
+    tipoFigura: Figura , 
+    radio?: number, 
+    base?: number, 
+    altura?: number
+): string => {
+    let area: number 
+
+    if(tipoFigura === 'circulo'){
+        if (radio === undefined) {
+            return 'Error: falta el radio para calcular el área del círculo.';
+          }
+        area = Math.PI * Math.pow(radio, 2);
+        return `El area del circulo es ${area.toFixed(2)}`
+    } else if(tipoFigura === 'rectangulo'){
+        if (base === undefined || altura === undefined) {
+            return 'Error: faltan base y/o altura para calcular el área del rectángulo.';
+          }
         area = base * altura
-        return `El area del rectangulo es ${area}`
+        return `El area del rectangulo es ${area.toFixed(2)}`
+    } else {
+        return 'Tipo de figura no válida.';
     }
 
 }
 console.log(calcularArea('circulo', 3))
+console.log(calcularArea('rectangulo', undefined, 4 ,5))
